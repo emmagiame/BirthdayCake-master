69663ccd921dac3de995616fa132d7127a28bf8a
giamello26 <giamello26@up.edu>
2023-09-26 13:26:04 -0700

    lab4 update #2 for group work

---
+++ app/src/main/java/cs301/birthdaycake/CakeController.java
@@ -0,0 +1,64 @@
+package cs301.birthdaycake;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class CakeController implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+    private CakeView view;
+    private CakeModel model;
+
+    CakeController(CakeView cakeView) {
+        view = cakeView;
+        model = view.getCakeModel();
+    }
+
+    @Override
+    public void onClick(View viewy) {
+        Log.d("CakeController", "onClick");
+        if(viewy instanceof Button)
+        {
+            Button bOB = (Button)viewy;
+            if (model.candleLit == false) {
+                model.candleLit = false;
+                bOB.setText("Relight");
+            } else if (model.candleLit == true) {
+                model.candleLit = true;
+                bOB.setText("Blow Out");
+            }
+            view.invalidate();
+        }
+    }
+
+    public void onCheckedChanged(View viewy) {
+        Log.d("CakeController", "onCheckedChanged");
+        if (model.hasCandles == true) {
+            model.hasCandles = false;
+        } else if (model.hasCandles == false) {
+            model.hasCandles = true;
+        }
+        view.invalidate();
+    }
+
+    //public void setOnSeekBarChangeListener(View viewy){
+    //    Log.d("CakeController", "setOnSeekBarChangeListener");
+    //    view.invalidate();
+    //}
+    @Override
+    public void onProgressChanged(SeekBar pickNumCandles, int progress, boolean fromUser) {
+        model.numCandles = progress;
+        view.invalidate();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+}
