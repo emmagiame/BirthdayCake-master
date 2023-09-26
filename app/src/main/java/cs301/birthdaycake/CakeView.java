bdbeb9c5f9d2872ac3dce9b289795c0b04ba4518
giamello26 <giamello26@up.edu>
2023-09-26 13:02:55 -0700

    lab4 update for group work

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -1,5 +1,7 @@
 package cs301.birthdaycake;
 
+import static android.util.Log.i;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,6 +10,11 @@ import android.util.AttributeSet;
 import android.view.SurfaceView;
 
 public class CakeView extends SurfaceView {
+    private CakeModel cakeModel = new CakeModel();
+
+    public CakeModel getCakeModel(){
+        return cakeModel;
+    }
 
     /* These are the paints we'll use to draw the birthday cake below */
     Paint cakePaint = new Paint();
@@ -71,14 +78,16 @@ public class CakeView extends SurfaceView {
     public void drawCandle(Canvas canvas, float left, float bottom) {
         canvas.drawRect(left, bottom - candleHeight, left + candleWidth, bottom, candlePaint);
 
-        //draw the outer flame
-        float flameCenterX = left + candleWidth/2;
-        float flameCenterY = bottom - wickHeight - candleHeight - outerFlameRadius/3;
-        canvas.drawCircle(flameCenterX, flameCenterY, outerFlameRadius, outerFlamePaint);
+        if(cakeModel.candleLit == true) {
+            //draw the outer flame
+            float flameCenterX = left + candleWidth / 2;
+            float flameCenterY = bottom - wickHeight - candleHeight - outerFlameRadius / 3;
+            canvas.drawCircle(flameCenterX, flameCenterY, outerFlameRadius, outerFlamePaint);
 
-        //draw the inner flame
-        flameCenterY += outerFlameRadius/3;
-        canvas.drawCircle(flameCenterX, flameCenterY, innerFlameRadius, innerFlamePaint);
+            //draw the inner flame
+            flameCenterY += outerFlameRadius / 3;
+            canvas.drawCircle(flameCenterX, flameCenterY, innerFlameRadius, innerFlamePaint);
+        }
 
         //draw the wick
         float wickLeft = left + candleWidth/2 - wickWidth/2;
@@ -119,10 +128,15 @@ public class CakeView extends SurfaceView {
         //Then a second cake layer
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
-        //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
-        drawCandle(canvas, cakeLeft + cakeWidth/4 - candleWidth/4, cakeTop);
+        if(cakeModel.hasCandles == true) {
+            //Now a candle in the center
+            int spacing = (int) (cakeWidth/(cakeModel.numCandles+1));
+            int i = 0;
+            for(i = 0; i < cakeModel.numCandles; i++){
+                drawCandle(canvas, cakeLeft+i*(spacing), cakeTop);
+            }
 
+        }
     }//onDraw
 
 }//class CakeView
--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -3,6 +3,10 @@ package cs301.birthdaycake;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+        CakeView cakeView = findViewById(R.id.cakeview);
+        CakeController cakeController = new CakeController(cakeView);
+        Button blowOutButton = this.findViewById(R.id.blowOutButton);
+        blowOutButton.setOnClickListener(cakeController);
+        SeekBar pickNumCandles = this.findViewById(R.id.pickNumCandles);
+        pickNumCandles.setOnSeekBarChangeListener(cakeController);
     }
+    public void goodbye(View button){
+        Log.i("button","Goodbye.");
+        finishAffinity();
+    }
+
 }
